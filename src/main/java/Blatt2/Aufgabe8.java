e137dfcd3400ba6f776efdbb4f4f24c546feb207
Viet <frostyfunta@gmail.com>
2021-04-28 14:16:28 +0200

    added myMatrixRandom method

--- src/main/java/Blatt2/Aufgabe8.java
+++ src/main/java/Blatt2/Aufgabe8.java
@@ -6,5 +6,13 @@ public class Aufgabe8 {
 
     }
 
-    private static
+    private static double[] myMatrixRandom(int x, int y) {
+        int i, j;
+        double[] matrix = new double[x][y];
+        for (i = 0; i < x; i++) {
+            for (j = 0; j < y; j++) {
+                matrix[i][j] = Math.random() * Double.MAX_VALUE;
+            }
+        }
+    }
 }
